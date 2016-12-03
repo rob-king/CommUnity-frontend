163d7fd270fd8e92f80f2b0fcef15ffa3099dbaa
Angel Valant <angelgithub@gmail.com>
2016-12-03 12:14:23 -0500

    adds listing form, faq information, and styling

--- js/app.js
+++ js/app.js
@@ -1,30 +1,41 @@
 angular
 .module("communityApp", ["ui.router", "ngResource", "ui.bootstrap"])
 .config(["$stateProvider", RouterFunction])
-.controller("indexController", [IndexControllerFunction])
+.factory("CommunityFactory", ["$resource", CommunityFactoryFunction])
+.controller("IndexController", ["CommunityFactory", IndexControllerFunction])
+
+function CommunityFactoryFunction($resource){
+  return $resource( "http://localhost:8080/");
+}
 
 function RouterFunction($stateProvider){
-  console.log("routerfunction")
+  console.log("log: router function works")
   $stateProvider.state("welcome", {
     url: "/",
     templateUrl: "js/ng-views/welcome.html",
-    controller: "indexController",
+    controller: "IndexController",
     controllerAs: "vm"
   });
   $stateProvider.state("listing",{
     url: "/listing.html",
     templateUrl: "js/ng-views/listing.html",
-    controller: "indexController",
+    controller: "IndexController",
+    controllerAs: "vm"
+  });
+  $stateProvider.state("show", {
+    url: "/show.html",
+    templateUrl: "js/ng-views/show.html",
+    controller: "IndexController",
     controllerAs: "vm"
   });
   $stateProvider.state("faq", {
     url: "/faq.html",
     templateUrl: "js/ng-views/faq.html",
-    controller: "indexController",
+    controller: "IndexController",
     controllerAs: "vm"
   })
 }
 //
-function IndexControllerFunction(){
-  console.log("indexController working")
+function IndexControllerFunction(CommunityFactory){
+  console.log("log: index controller works")
 }
